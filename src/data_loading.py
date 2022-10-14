b0f593a01a7ea69fc6588af938f6c7eb810e7eb9
Adriana Solis <60237877+solisa986@users.noreply.github.com>
2022-10-13 23:19:27 -0400

    working on implementing charts with data

--- src/data_loading.py
+++ src/data_loading.py
@@ -2,12 +2,16 @@
 
 import streamlit as st
 import pandas as pd
-import numpy as np
+import shap
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.ensemble import RandomForestRegressor
+import plotly.express as px
 
 st.title('Movies123forMe - A Personalized Movie Selector')
 
-DATA_FULL_PATH = ('C:\\Users\\solis\\OneDrive\\Documents\\comp\movie_data\\Full Extract\\')
-DATA_BASIC_PATH = ('C:\\Users\\solis\\OneDrive\\Documents\\comp\\movie_data\\Basic Extract\\')
+DATA_FULL_PATH = ('C:\\Users\\solis\\OneDrive\\Documents\\comp\\Movies123forMe\\src\\movie_data\\full\\')
+DATA_BASIC_PATH = ('C:\\Users\\solis\\OneDrive\\Documents\\comp\\Movies123forMe\\src\\movie_data\\basic\\')
 
 # BASIC PACKAGE PART OF THE CODE
 
@@ -173,7 +177,7 @@ movie23 = load_data_full(10000)[22]
 data_load_state.text("Done!")
 
 # basic package display
-def display():
+def display(choice):
     menu = ["Home", "Basic", "Full"]
     choice = st.sidebar.selectbox("Menu", menu)
     if choice == 'Basic':
@@ -290,3 +294,82 @@ def display():
         st.write("\n---\n")
     else:
         st.subheader("Home")
+
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
+if display(choice='Home'):
+        # loading the data into a dataset
+        data_basic_load = load_data_basic(100)
+        data_full_load = load_data_full(100)
+        x1 = pd.DataFrame(data_basic_load.data, columns=data_basic_load.feature_names)
+        x2 = pd.DataFrame(data_full_load.data, columns=data_full_load.feature_names)
+        y1 = pd.DataFrame(data_basic_load.target, column=["MEDV"])
+        y2 = pd.DataFrame(data_full_load.target, column=["MEDV"])
+
+        df_basic = pd.DataFrame(data_basic_load.data, columns=data_basic_load.feature_names)
+        df_full = pd.DataFrame(data_full_load.data, columns=data_full_load.feature_names)
+        df_basic['target'] = pd.Series(data_basic_load.target)
+        df_full['target'] = pd.Series(data_full_load.target)
+        st.write(df_basic)
+        st.write(df_full)
+        # visualizing the dataset
+        chart = st.sidebar.selectbox(
+            label = "Select the type of chart",
+            options = ["Scatterplot", "Lineplots", "Histogram", "Boxplot"]
+        )
+        numeric_column1 = list(df_basic.select_dtypes(['float', 'int']).columns)
+        numeric_column2 = list(df_full.select_dtypes(['float', 'int']).columns)
+
+        if chart == 'Scatterplot':
+            st.sidebar.subheader('Scatterplot Settings')
+            try:
+                x_vals1 = st.sidebar.selectbox('X Axis', options=numeric_column1)
+                y_vals1 = st.sidebar.selectbox('Y Axis', options=numeric_column1)
+                x_vals2 = st.sidebar.selectbox('X Axis', options=numeric_column2)
+                y_vals2 = st.sidebar.selectbox('Y Axis', options=numeric_column2)
+                plot1 = px.scatter(data_frame=df_basic, x=x_vals1, y=y_vals1)
+                plot2 = px.scatter(data_frame=df_full, x=x_vals2, y=y_vals2)
+                st.write(plot1)
+                st.write(plot2)
+            except Exception as e:
+                print(e)
+        
+        if chart == 'Histogram':
+            st.sidebar.subheader('Histogram Settings')
+            try:
+                x_vals1 = st.sidebar.selectbox('X Axis', options=numeric_column1)
+                x_vals2 = st.sidebar.selectbox('X Axis', options=numeric_column2)
+                plot1 = px.histogram(data_frame=df_basic, x=x_vals1)
+                plot2 = px.histogram(data_frame=df_full, x=x_vals2)
+                st.write(plot1)
+                st.write(plot2)
+            except Exception as e:
+                print(e)
+        
+        if chart == 'Lineplot':
+            st.sidebar.subheader('Lineplot Settings')
+            try:
+                x_vals1 = st.sidebar.selectbox('X Axis', options=numeric_column1)
+                y_vals1 = st.sidebar.selectbox('Y Axis', options=numeric_column1)
+                x_vals2 = st.sidebar.selectbox('X Axis', options=numeric_column2)
+                y_vals2 = st.sidebar.selectbox('Y Axis', options=numeric_column2)
+                plot1 = px.line(data_frame=df_basic, x=x_vals1, y=y_vals1)
+                plot2 = px.line(data_frame=df_full, x=x_vals2, y=y_vals2)
+                st.write(plot1)
+                st.write(plot2)
+            except Exception as e:
+                print(e)
+        
+        if chart == 'Boxplot':
+            st.sidebar.subheader('Boxplot Settings')
+            try:
+                x_vals1 = st.sidebar.selectbox('X Axis', options=numeric_column1)
+                y_vals1 = st.sidebar.selectbox('Y Axis', options=numeric_column1)
+                x_vals2 = st.sidebar.selectbox('X Axis', options=numeric_column2)
+                y_vals2 = st.sidebar.selectbox('Y Axis', options=numeric_column2)
+                plot1 = px.box(data_frame=df_basic, x=x_vals1, y=y_vals1)
+                plot2 = px.box(data_frame=df_full, x=x_vals2, y=y_vals2)
+                st.write(plot1)
+                st.write(plot2)
+            except Exception as e:
+                print(e)
--- src/machine_learning.py
+++ src/machine_learning.py
@@ -1,48 +1,33 @@
 import streamlit as st
-import data_loading
 import pandas as pd
-import numpy as np
-import joblib
-import os
+import shap
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.ensemble import RandomForestRegressor
+import plotly.express as px
+import data_loading as dl
 
-def load_model(model_file):
-    model = joblib.load(open(os.path.join(model_file), "rb"))
-    return model
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
-data_classifier = load_model('src/movie_data/basic/movie_ratings.csv')
+if dl.display('Home'):
+        st.write("Movies123ForMe")
+        # loading the data into a dataset
+        data_basic_load = dl.load_data_basic()
+        data_full_load = dl.load_data_full()
+        x1 = pd.DataFrame(data_basic_load.data, columns=data_basic_load.feature_names)
+        x2 = pd.DataFrame(data_full_load.data, columns=data_full_load.feature_names)
+        y1 = pd.DataFrame(data_basic_load.target, column=["MEDV"])
+        y2 = pd.DataFrame(data_full_load.target, column=["MEDV"])
 
-def plot_prediction(term):
-    pred_prob_df = pd.DataFrame({'Probabilities':data_classifier.predict_prob_df([term])[0], 'Classes':data_classifier})
-    c = alt.Chart(pred_prob_df).mark_bar().encode(
-        x = 'Classes',
-        y = 'Probabilities',
-        color = 'Classes'
-    )
-    st.altair_chart(c)
-
-def main():
-    display = data_loading.display()
-    with st.form(key='myform'):
-        predict = st.text_area("Enter movie data here: ")
-        submit = st.form_submit_button(label='predict')
-    
-    if submit:
-        col1, col2 = st.columns(2)
-        prediction = data_classifier.predict([predict])
-        pred_proba = data_classifier.predict_proba([predict])
-        probabilities = dict(zip(data_classifier.classes_,pred_proba[0]))
-
-        # display results
-        with col1:
-            st.info("Original Data")
-            st.text(predict)
-            st.write(prediction)
-            st.write(probabilities)
-
-        # Display Plots
-        with col2:
-            st.info("Probability Plots")
-            
-
-if __name__ == '__main__':
-    main()
+        df_basic = pd.DataFrame(data_basic_load.data, columns=data_basic_load.feature_names)
+        df_full = pd.DataFrame(data_full_load.data, columns=data_full_load.feature_names)
+        df_basic['target'] = pd.Series(data_basic_load.target)
+        df_full['target'] = pd.Series(data_full_load.target)
+        st.write(df_basic)
+        st.write(df_full)
+        # visualizing the dataset
+        chart = st.sidebar.selectbox(
+            label = "Select the type of chart",
+            options = ["Scatterplot", "Lineplots", "Histogram", "Boxplot"]
+        )
+        # numeric_column = list(df_basic.select_dtypes(['float', 'int']).colu)
