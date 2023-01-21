be7d5e47c70febc75656d0a1c95371ba392d9645
Adriana Solis <60237877+solisa986@users.noreply.github.com>
2023-01-21 14:07:51 -0500

    added etl to main file

--- src/main.py
+++ src/main.py
@@ -1,15 +1,18 @@
 import streamlit as st
 from working_files import data_loading
+from working_files import etl
 
 st.title("Welcome to the Movie Analysis Experience :)")
 
 def main():
-    options = ['Home', 'Data Loading', ]
+    options = ['Home', 'Data Loading', 'ETL']
     choice = st.sidebar.selectbox("Choose an analysis option", options, key = "298")
 
     if choice == 'Home':
         st.write("")
     if choice == 'Data Loading':
         data_loading.display()
+    if choice == 'ETL':
+        etl.clean_data()
 
 main()
