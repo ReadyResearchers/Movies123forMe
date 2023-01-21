aec711b5417b50c41ee5432f2fdba6a237b457ca
Adriana Solis <60237877+solisa986@users.noreply.github.com>
2023-01-21 14:22:40 -0500

    add machine learning examples to main

--- src/main.py
+++ src/main.py
@@ -1,11 +1,13 @@
 import streamlit as st
 from working_files import data_loading
 from working_files import etl
+from working_files import ml_model
+from working_files import machine_learning
 
 st.title("Welcome to the Movie Analysis Experience :)")
 
 def main():
-    options = ['Home', 'Data Loading', 'ETL']
+    options = ['Home', 'Data Loading', 'ETL', 'ML Model', 'Machine Learning']
     choice = st.sidebar.selectbox("Choose an analysis option", options, key = "298")
 
     if choice == 'Home':
@@ -14,5 +16,9 @@ def main():
         data_loading.display()
     if choice == 'ETL':
         etl.clean_data()
+    if choice == 'ML Model':
+        ml_model.machine_model()
+    if choice == 'Machine Learning':
+        machine_learning.m_learning()
 
 main()
--- src/working_files/machine_learning.py
+++ src/working_files/machine_learning.py
@@ -9,76 +9,79 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-data = pd.read_csv('src\\movie_data\\movie_data\\movie_data.csv')
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
-# data processing
-data1 = data.drop(['movie_name', 'rating', 'genre', 'creative_type', 'source', 'production_method', 'domestic_box_office', 'international_box_office'], axis=1)
-data2 = data1.dropna()
-x = data2.drop(['total_revenue'], axis=1)
-y = data2['total_revenue']
+def m_learning():
+    data = pd.read_csv('src\\movie_data\\movie_data\\movie_data.csv')
 
-# data splitting
-X_train, X_test, y_train, y_test = train_test_split(
-    x, y, test_size=0.2, random_state=42)
+    # data processing
+    data1 = data.drop(['movie_name', 'rating', 'genre', 'creative_type', 'source', 'production_method', 'domestic_box_office', 'international_box_office'], axis=1)
+    data2 = data1.dropna()
+    x = data2.drop(['production_budget'], axis=1)
+    y = data2['production_budget']
 
-# model building
-lr = LinearRegression()
-lr.fit(X_train, y_train)
+    # data splitting
+    X_train, X_test, y_train, y_test = train_test_split(
+        x, y, test_size=0.2, random_state=42)
 
-y_lr_train_pred = lr.predict(X_train)
-y_lr_test_pred = lr.predict(X_test)
+    # model building
+    lr = LinearRegression()
+    lr.fit(X_train, y_train)
 
-lr_train_mse = mean_squared_error(y_train, y_lr_train_pred)
-lr_train_r2 = r2_score(y_train, y_lr_train_pred)
-lr_test_mse = mean_squared_error(y_test, y_lr_test_pred)
-lr_test_r2 = r2_score(y_test, y_lr_test_pred)
+    y_lr_train_pred = lr.predict(X_train)
+    y_lr_test_pred = lr.predict(X_test)
 
-lr_results = pd.DataFrame(['Linear regression',lr_train_mse, lr_train_r2, lr_test_mse, lr_test_r2]).transpose()
-lr_results.columns = ['Method','Training MSE','Training R2','Test MSE','Test R2']
+    lr_train_mse = mean_squared_error(y_train, y_lr_train_pred)
+    lr_train_r2 = r2_score(y_train, y_lr_train_pred)
+    lr_test_mse = mean_squared_error(y_test, y_lr_test_pred)
+    lr_test_r2 = r2_score(y_test, y_lr_test_pred)
 
-# random forest
-rf = RandomForestRegressor(max_depth=2, random_state=42)
-rf.fit(X_train, y_train)
+    lr_results = pd.DataFrame(['Linear regression',lr_train_mse, lr_train_r2, lr_test_mse, lr_test_r2]).transpose()
+    lr_results.columns = ['Method','Training MSE','Training R2','Test MSE','Test R2']
 
-y_rf_train_pred = rf.predict(X_train)
-y_rf_test_pred = rf.predict(X_test)
+    # random forest
+    rf = RandomForestRegressor(max_depth=2, random_state=42)
+    rf.fit(X_train, y_train)
 
-# model performance
-rf_train_mse = mean_squared_error(y_train, y_rf_train_pred)
-rf_train_r2 = r2_score(y_train, y_rf_train_pred)
-rf_test_mse = mean_squared_error(y_test, y_rf_test_pred)
-rf_test_r2 = r2_score(y_test, y_rf_test_pred)
+    y_rf_train_pred = rf.predict(X_train)
+    y_rf_test_pred = rf.predict(X_test)
 
-rf_results = pd.DataFrame(['Random forest',rf_train_mse, rf_train_r2, rf_test_mse, rf_test_r2]).transpose()
-rf_results.columns = ['Method','Training MSE','Training R2','Test MSE','Test R2']
+    # model performance
+    rf_train_mse = mean_squared_error(y_train, y_rf_train_pred)
+    rf_train_r2 = r2_score(y_train, y_rf_train_pred)
+    rf_test_mse = mean_squared_error(y_test, y_rf_test_pred)
+    rf_test_r2 = r2_score(y_test, y_rf_test_pred)
 
-
-# using a regressor
-et = ExtraTreeRegressor(random_state=42)
-et.fit(X_train, y_train)
-
-y_et_train_pred = et.predict(X_train)
-y_et_test_pred = et.predict(X_test)
-
-et_train_mse = mean_squared_error(y_train, y_et_train_pred)
-et_train_r2 = r2_score(y_train, y_et_train_pred)
-et_test_mse = mean_squared_error(y_test, y_et_test_pred)
-et_test_r2 = r2_score(y_test, y_et_test_pred)
-
-et_results = pd.DataFrame(['Extra Tree',et_train_mse, et_train_r2, et_test_mse, et_test_r2]).transpose()
-et_results.columns = ['Method','Training MSE','Training R2','Test MSE','Test R2']
+    rf_results = pd.DataFrame(['Random forest',rf_train_mse, rf_train_r2, rf_test_mse, rf_test_r2]).transpose()
+    rf_results.columns = ['Method','Training MSE','Training R2','Test MSE','Test R2']
 
 
-# display in one table
-st.write(pd.concat([lr_results, rf_results, et_results]))
+    # using a regressor
+    et = ExtraTreeRegressor(random_state=42)
+    et.fit(X_train, y_train)
 
-# data visualization
-plt.figure(figsize=(5,5))
-plt.scatter(x=y_train, y=y_lr_train_pred, c="#7CAE00", alpha=0.3)
-z = np.polyfit(y_train, y_lr_train_pred, 1)
-p = np.poly1d(z)
-plt.plot(y_train,p(y_train),"#F8766D")
-plt.ylabel('Predicted LogS')
-plt.xlabel('Experimental LogS')
+    y_et_train_pred = et.predict(X_train)
+    y_et_test_pred = et.predict(X_test)
 
-st.pyplot(plt.plot())
+    et_train_mse = mean_squared_error(y_train, y_et_train_pred)
+    et_train_r2 = r2_score(y_train, y_et_train_pred)
+    et_test_mse = mean_squared_error(y_test, y_et_test_pred)
+    et_test_r2 = r2_score(y_test, y_et_test_pred)
+
+    et_results = pd.DataFrame(['Extra Tree',et_train_mse, et_train_r2, et_test_mse, et_test_r2]).transpose()
+    et_results.columns = ['Method','Training MSE','Training R2','Test MSE','Test R2']
+
+
+    # display in one table
+    st.write(pd.concat([lr_results, rf_results, et_results]))
+
+    # data visualization
+    plt.figure(figsize=(5,5))
+    plt.scatter(x=y_train, y=y_lr_train_pred, c="#7CAE00", alpha=0.3)
+    z = np.polyfit(y_train, y_lr_train_pred, 1)
+    p = np.poly1d(z)
+    plt.plot(y_train,p(y_train),"#F8766D")
+    plt.ylabel('Predicted LogS')
+    plt.xlabel('Experimental LogS')
+
+    st.pyplot(plt.plot())
