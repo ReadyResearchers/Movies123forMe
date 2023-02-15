7256e4019a7053b97ffcad5c8109874768918218
Adriana Solis <60237877+solisa986@users.noreply.github.com>
2023-02-14 20:23:10 -0500

    changed the linear kernel to cosine similarity

--- src/pages/functions/text_classification.py
+++ src/pages/functions/text_classification.py
@@ -6,7 +6,10 @@ import re
 from pages.functions import B_etl
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
-from sklearn.metrics import accuracy_score
+from sklearn.feature_extraction.text import CountVectorizer
+import nltk
+from nltk.stem.porter import PorterStemmer
+from sklearn.metrics.pairwise import cosine_similarity
 
 ## importing necessary files
 duplicates = 'C:\\Users\\solis\\OneDrive\\Documents\\comp\\Movies123forMe\\src\\movie_search.csv'
@@ -28,6 +31,120 @@ data.columns = ['Title','Year','Rated','Released','Runtime','Genre','Director',
 
 data = data.drop_duplicates(subset = ['Title'], keep='first').reset_index()
 
+
+def by_all():
+    # doing data preprocessing
+    movies = data[['imdbID', 'Title', 'Plot', 'Genre', 'Actors', 'Director', 'Writer', 'Rated']]
+
+    #st.write(movies.isnull().sum())
+
+    movies.dropna(inplace=True)
+
+    movies = movies.drop_duplicates().reset_index()
+
+    movies['tags'] = movies['Genre'] + " " +  movies['Plot'] + " " + movies['Actors'] + " " + movies['Director'] + " " + movies['Writer'] + " " + movies['Rated']
+
+    new_df = movies[['imdbID', 'Title', 'tags']]
+    # checking for duplicate data
+
+    cv = CountVectorizer(max_features=5000, stop_words='english')
+    vectors = cv.fit_transform(new_df['tags']).toarray()
+    ps = PorterStemmer()
+
+    def stem(text):
+        y = []
+        for i in text.split():
+            y.append(ps.stem(i))
+        return " ".join(y)
+
+    new_df['tags'] = new_df['tags'].apply(stem)
+    similarity = cosine_similarity(vectors)
+    indices = pd.Series(new_df.index, index=new_df['Title']).drop_duplicates()
+
+    def get_recommendations(title, cosine_sim=similarity):
+        idx = indices[title]
+        sim_scores = list(enumerate(cosine_sim[idx]))
+        sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+        sim_scores = sim_scores[1:11]
+        movie_indices = [i[0] for i in sim_scores]
+        return pd.DataFrame(new_df[['Title', 'tags']].iloc[movie_indices])
+
+    movie_list = new_df['Title'].values
+
+    selected_movie = st.selectbox( "Type or select a movie from the dropdown", movie_list, key=movie_list)
+
+    if st.button('Show Recommendation'):
+        recommended_movie_names = get_recommendations(selected_movie)
+        a = recommended_movie_names.values[0][0]
+        b = recommended_movie_names.values[1][0]
+        c = recommended_movie_names.values[2][0]
+        d = recommended_movie_names.values[3][0]
+        if a:
+            url = f'http://www.omdbapi.com/?t={a}&apikey=4482116e'
+            re = requests.get(url)
+            re = re.json()
+            try:
+                col1, col2 = st.columns([1, 2])
+                with col1:
+                    st.image(re['Poster'])
+                with col2:
+                    st.subheader(re['Title'])
+                    st.caption(f"Genre: {re['Genre']} | Year: {re['Year']} | Rated: {re['Rated']} | Released: {re['Released']}")
+                    st.write(re['Plot'])
+                    st.text(f"Rating: {re['imdbRating']}")
+                    st.progress(float(re['imdbRating']) / 10)
+            except:
+                st.error("No movie with that title found in the API Database YET -- add it to our database using the Movie Search feature!")
+        if b:
+            url = f'http://www.omdbapi.com/?t={b}&apikey=4482116e'
+            re = requests.get(url)
+            re = re.json()
+            try:
+                col1, col2 = st.columns([1, 2])
+                with col1:
+                    st.image(re['Poster'])
+                with col2:
+                    st.subheader(re['Title'])
+                    st.caption(f"Genre: {re['Genre']} | Year: {re['Year']} | Rated: {re['Rated']} | Released: {re['Released']}")
+                    st.write(re['Plot'])
+                    st.text(f"Rating: {re['imdbRating']}")
+                    st.progress(float(re['imdbRating']) / 10)
+            except:
+                st.error("No movie with that title found in the API Database YET -- add it to our database using the Movie Search feature!")
+        if c:
+            url = f'http://www.omdbapi.com/?t={c}&apikey=4482116e'
+            re = requests.get(url)
+            re = re.json()
+            try:
+                col1, col2 = st.columns([1, 2])
+                with col1:
+                    st.image(re['Poster'])
+                with col2:
+                    st.subheader(re['Title'])
+                    st.caption(f"Genre: {re['Genre']} | Year: {re['Year']} | Rated: {re['Rated']} | Released: {re['Released']}")
+                    st.write(re['Plot'])
+                    st.text(f"Rating: {re['imdbRating']}")
+                    st.progress(float(re['imdbRating']) / 10)
+            except:
+                st.error("No movie with that title found in the API Database YET -- add it to our database using the Movie Search feature!")
+        if d:
+            url = f'http://www.omdbapi.com/?t={d}&apikey=4482116e'
+            re = requests.get(url)
+            re = re.json()
+            try:
+                col1, col2 = st.columns([1, 2])
+                with col1:
+                    st.image(re['Poster'])
+                with col2:
+                    st.subheader(re['Title'])
+                    st.caption(f"Genre: {re['Genre']} | Year: {re['Year']} | Rated: {re['Rated']} | Released: {re['Released']}")
+                    st.write(re['Plot'])
+                    st.text(f"Rating: {re['imdbRating']}")
+                    st.progress(float(re['imdbRating']) / 10)
+            except:
+                st.error("No movie with that title found in the API Database YET -- add it to our database using the Movie Search feature!")
+
+
 def by_plot():
     # checking for duplicate data
     movies = ['Title', 'Plot']
@@ -39,7 +156,7 @@ def by_plot():
     movie_data['Plot'] = movie_data['Plot'].fillna('')
     tfidf_matrix = tfidf.fit_transform(movie_data['Plot'])
 
-    cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+    cosine_sim = cosine_similarity(tfidf_matrix)
 
     indices = pd.Series(movie_data.index, index=movie_data['Title']).drop_duplicates()
 
@@ -139,7 +256,7 @@ def by_actor():
     movie_data['Actors'] = movie_data['Actors'].fillna('')
     tfidf_matrix = tfidf.fit_transform(movie_data['Actors'])
 
-    cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+    cosine_sim = cosine_similarity(tfidf_matrix)
 
     indices = pd.Series(movie_data.index, index=movie_data['Title']).drop_duplicates()
 
@@ -237,7 +354,7 @@ def by_director():
     movie_data['Director'] = movie_data['Director'].fillna('')
     tfidf_matrix = tfidf.fit_transform(movie_data['Director'])
 
-    cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+    cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
 
     indices = pd.Series(movie_data.index, index=movie_data['Title']).drop_duplicates()
 
@@ -335,7 +452,7 @@ def by_genre():
     movie_data['Genre'] = movie_data['Genre'].fillna('')
     tfidf_matrix = tfidf.fit_transform(movie_data['Genre'])
 
-    cosine_sim = linear_kernel(tfidf_matrix, tfidf_matrix)
+    cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
 
     indices = pd.Series(movie_data.index, index=movie_data['Title']).drop_duplicates()
 
@@ -426,7 +543,7 @@ def by_genre():
 
 def category():
     st.header("Movie Recommendation System")
-    choose = ['Plot', 'Director', 'Actors', 'Genre']
+    choose = ['Plot', 'Director', 'Actors', 'Genre', 'All']
     choices = st.selectbox("Choose which type of movie grouping for recommendation: ", choose)
 
     if choices == 'Plot':
@@ -437,5 +554,7 @@ def category():
         by_actor()
     if choices == 'Genre':
         by_genre()
+    if choices == 'All':
+        by_all()
 
 category()
