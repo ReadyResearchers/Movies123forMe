f63c71b8c8feb3aee846d44f30495698c7dc7aaa
Adriana Solis <60237877+solisa986@users.noreply.github.com>
2023-03-15 18:16:58 -0400

    reorganized file structure

---
+++ pages/E_regressions.py
@@ -0,0 +1,59 @@
+import streamlit as st
+from pages import B_etl
+
+import plotly.express as px
+
+st.markdown("# Welcome to the Movie Analysis Experience 🎈")
+st.sidebar.markdown("# Subpage 6 🎈")
+
+
+def regression():
+    """Run linear regression on opus data."""
+    opus = B_etl.clean_data()[0]
+    st.write(opus.head())
+    # finding the mean production budget of a successful movie
+    #for i in opus['movie_success']:
+        #if i == 1:
+            #print(opus['production_budget'].mean())
+    col = opus.columns
+    columns = {1: opus.columns[0], 2: opus.columns[1], 3: opus.columns[2],
+        4: opus.columns[3], 5: opus.columns[4], 6: opus.columns[5],
+        7: opus.columns[6], 8: opus.columns[7], 9: opus.columns[8],
+        10: opus.columns[9], 11: opus.columns[10], 12: opus.columns[11],
+        13: opus.columns[12], 14: opus.columns[13], 15: opus.columns[14],
+        16: opus.columns[15], 17: opus.columns[16], 18: opus.columns[17],
+        19: opus.columns[18]}
+    category = st.selectbox("Select a category to analyze", list(columns.keys()), format_func=lambda x: columns[x])
+    x_val = st.selectbox("X-value: ", col[:])
+    y_val = st.selectbox("Y-val: ", col[:8])
+    if x_val == y_val:
+        x_val = opus.columns[1]
+        y_val = opus.columns[3]
+
+    # scatterplot chart
+    fig = px.scatter(opus, x = x_val, y = y_val, color = columns[category], 
+                    trendline = 'ols', title = f"Scatterplot of {y_val} on {x_val}")
+    fig.data[1].line.color = 'red'
+    fig.update_xaxes(rangeslider_visible = True)
+    st.plotly_chart(fig)
+    res = px.get_trendline_results(fig)
+    trendline = res['px_fit_results'].iloc[0]
+    st.write(trendline.summary())
+    # pie chart
+    fig1 = px.pie(opus, values = x_val, names = y_val, 
+                title = f'Percentage of movie "{y_val}" in "{x_val}"')
+    fig1.update_traces(textinfo="percent+value")
+    st.plotly_chart(fig1)
+
+    # bar chart
+    fig2 = px.bar(opus, x = x_val, y = y_val, color = columns[category],
+    title = f'Bar chart of {x_val} on {y_val}')
+    st.plotly_chart(fig2)
+
+    #might need to work on this to get the func to work
+    fig3 = px.bar(opus, x = x_val, color = columns[category],
+    title = f'Bar chart of the count of {x_val}')
+    st.plotly_chart(fig3)
+
+regression()
+#os.remove(html_fname)
---
+++ pages/function_folder/A_data_loading.py
@@ -0,0 +1,134 @@
+import streamlit as st
+import pandas as pd
+
+from pathlib import Path
+import plotly.express as px
+
+# st.title('Movies123forMe - A Personalized Movie Selector')
+
+DATA_OPUS = ('C:\\Users\\solis\\OneDrive\\Documents\\comp\\Movies123forMe\\movie_data\\movie_data\\')
+DATA_NETFLIX = ('C:\\Users\\solis\\OneDrive\\Documents\\comp\\Movies123forMe\\movie_data\\netflix\\')
+DATA_HULU = ('C:\\Users\\solis\\OneDrive\\Documents\\comp\\Movies123forMe\\movie_data\\hulu\\')
+DATA_PRIME = ('C:\\Users\\solis\\OneDrive\\Documents\\comp\\Movies123forMe\\movie_data\\prime\\')
+DATA_DISNEY = ('C:\\Users\\solis\\OneDrive\\Documents\\comp\\Movies123forMe\\movie_data\\disney+\\')
+DATA_IMDB = ('C:\\Users\\solis\\OneDrive\\Documents\\comp\\Movies123forMe\\movie_data\\imdb\\')
+
+# OPUS PART OF THE CODE
+# creating text element to show the loading of the data in the app
+
+@st.cache_data
+def load_data_opus(nrows):
+    data = pd.read_csv(DATA_OPUS + 'movie_data.csv', nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    # setting up pandas dataframe for all of the files
+    data.rename(lowercase, axis='columns', inplace=True)
+    return data
+
+@st.cache_data
+# NETFLIX PART OF THE CODE
+def load_data_netflix(nrows):
+    data1 = pd.read_csv(DATA_NETFLIX + 'archive\\netflix_titles.csv', nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    # setting up pandas dataframe for all of the files
+    data1.rename(lowercase, axis='columns', inplace=True)
+    return data1
+
+@st.cache_data
+# HULU PART OF THE CODE
+def load_data_hulu(nrows):
+    data2 = pd.read_csv(DATA_HULU + 'archive\\hulu_titles.csv', nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    # setting up pandas dataframe for all of the files
+    data2.rename(lowercase, axis='columns', inplace=True)
+    return data2
+
+@st.cache_data
+# PRIME PART OF THE CODE
+def load_data_prime(nrows):
+    data3 = pd.read_csv(DATA_PRIME + 'archive\\amazon_prime_titles.csv', nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    # setting up pandas dataframe for all of the files
+    data3.rename(lowercase, axis='columns', inplace=True)
+    return data3
+
+@st.cache_data
+# DISNEY+ PART OF THE CODE
+def load_data_disney(nrows):
+    data4 = pd.read_csv(DATA_DISNEY + 'archive\\disney_plus_titles.csv', nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    # setting up pandas dataframe for all of the files
+    data4.rename(lowercase, axis='columns', inplace=True)
+    return data4
+# IMDB PACKAGE PART OF THE CODE
+list_names_imdb = ['title.akas', 'title.basics', 'title.crew', 'title.episode', 'title.principals',
+'title.ratings', 'name.basics']
+
+# creating text element to show the loading of the data in the app
+@st.cache_data
+def load_data_imdb(nrows):
+    title_basics = pd.read_table(DATA_IMDB + list_names_imdb[1] + '.tsv\\data.tsv', nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    # setting up pandas dataframe for all of the files
+    title_basics.rename(lowercase, axis='columns', inplace=True)
+    return title_basics
+
+
+# basic package display
+def display():
+    menu = ["Home", "Opus", "Netflix", "Hulu", "Disney+", "Prime", "IMDB", "Plots"]
+    choice = st.selectbox("Menu", menu, key=menu)
+    # imdb display
+    if choice == 'Opus':
+        st.subheader("Raw data for Opus Data package:")
+        st.write(load_data_opus(10000))
+        st.write("\n---\n")
+    if choice == 'Netflix':
+        st.subheader("Raw data for Netflix Data package:")
+        st.write(load_data_netflix(10000))
+        st.write("\n---\n")
+    if choice == 'Hulu':
+        st.subheader("Raw data for Hulu Data package:")
+        st.write(load_data_hulu(10000))
+        st.write("\n---\n")
+    if choice == 'Disney+':
+        st.subheader("Raw data for Disney+ Data package:")
+        st.write(load_data_disney(10000))
+        st.write("\n---\n")
+    if choice == 'Prime':
+        st.subheader("Raw data for Prime Data package:")
+        st.write(load_data_prime(10000))
+        st.write("\n---\n")
+    if choice == 'IMDB':
+        st.subheader(f"Raw data for IMDB Data Package:")
+        st.write(load_data_imdb(10000))
+        st.write("\n---\n")
+    if choice == 'Plots':
+        st.subheader("Demo for Movies123ForMe Analysis")
+        # loading the data into a dataset
+        data = load_data_opus(10000)
+        df = pd.DataFrame(data.values, columns=data.columns)
+        st.write(df)
+        # visualizing the dataset
+        chart = st.sidebar.selectbox(
+            label = "Select the type of chart",
+            options = ["Scatterplot", "Lineplots", "Histogram", "Boxplot"]
+        )
+        if chart == 'Scatterplot':
+            # st.sidebar.subheader('Scatterplot Settings')
+            scatter_fig1 = (px.scatter(x = data['production_budget'], y = data['rating'], title = "Movie Revenue by Rating"))
+            scatter_fig1.update_layout(xaxis_title = "Movie Budget", yaxis_title = "Movie Rating")
+            st.write(scatter_fig1)
+        if chart == 'Lineplots':
+            line_fig1 = (px.line(x = data['production_budget'], y = data['rating'], title = "Movie Revenue by Rating"))
+            line_fig1.update_layout(xaxis_title = "Movie Budget", yaxis_title = "Movie Rating")
+            st.write(line_fig1)
+        if chart == 'Histogram':
+            hist_fig1 = (px.histogram(x = data['production_budget'], y = data['rating'], title = "Movie Revenue by Rating"))
+            hist_fig1.update_layout(xaxis_title = "Movie Budget", yaxis_title = "Movie Rating")
+            st.write(hist_fig1)
+        if chart == 'Boxplot':
+            box_fig1 = (px.box(x = data['production_budget'], y = data['rating'], title = "Movie Revenue by Rating"))
+            box_fig1.update_layout(xaxis_title = "Movie Budget", yaxis_title = "Movie Rating")
+            st.write(box_fig1)
+
+display()
