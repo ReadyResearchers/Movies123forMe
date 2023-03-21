40cde44ca0addaf5da724235755646938e6f1b54
Adriana Solis <60237877+solisa986@users.noreply.github.com>
2023-03-21 14:09:18 -0400

    checking if linting is done with

--- main.py
+++ main.py
@@ -1,30 +1,17 @@
 """Main Method for the Interactive Movie Recommendation app."""
 
-import streamlit as st # pylint: disable=E0401, C0413
-import pandas as pd # pylint: disable=E0401, C0413
-from pages import clean_data # pylint: disable=E0401, C0413
+import streamlit as st # pylint: disable=E0401, C0413, C0411
+import pandas as pd # pylint: disable=E0401, C0413, C0411
 # from pages.function_folder import merge
-from pages.function_folder import text_classification # pylint: disable=E0401, C0413
-from sklearn.model_selection import train_test_split # pylint: disable=E0401, C0413
-from pages import G_machine_learning # pylint: disable=E0401, C0413
+from pages.function_folder import text_classification # pylint: disable=E0401, C0413, C0411
+from pages import G_machine_learning # pylint: disable=E0401, C0413, C0411, C0412
 
-import numpy as np # pylint: disable=E0401, C0413
-import joblib # pylint: disable=E0401, C0413
-import requests # pylint: disable=E0401, C0413
-import re # pylint: disable=E0401, C0413
-import nltk # pylint: disable=E0401, C0413
-from sklearn.feature_extraction.text import TfidfVectorizer # pylint: disable=E0401, C0413
-from sklearn.metrics.pairwise import cosine_similarity # pylint: disable=E0401, C0413
-nltk.download('stopwords')
-nltk.download('punkt')
-from nltk import word_tokenize # pylint: disable=E0401, C0413
-from nltk.corpus import stopwords # pylint: disable=E0401, C0413
-import pickle # pylint: disable=E0401, C0413
-from streamlit import session_state as session # pylint: disable=E0401, C0413
-from functools import reduce # pylint: disable=E0401, C0413
-import json # pylint: disable=E0401, C0413
-import csv # pylint: disable=E0401, C0413
-import pathlib # pylint: disable=E0401, C0413
+import numpy as np # pylint: disable=E0401, C0413, C0411
+import joblib # pylint: disable=E0401, C0413, C0411
+import requests # pylint: disable=E0401, C0413, C0411
+import nltk # pylint: disable=E0401, C0413, C0411
+from functools import reduce # pylint: disable=E0401, C0413, C0411
+import json # pylint: disable=E0401, C0413, C0411
 
 st.markdown("# Welcome to the Movie Analysis Experience 🎈")
 st.sidebar.markdown("# Main Page 🎈")
@@ -32,7 +19,7 @@ st.sidebar.markdown("# Main Page 🎈")
 data = pd.read_csv("merged_data.csv")
 
 @st.cache_data
-def predict(dataa):
+def predict(data):
     """Initial set up function to import the machine learning models."""
     logreg_filename = "C:/Users/solis/OneDrive/Documents/comp/Movies123forMe/logreg_model.sav"
     lr_filename = "C:/Users/solis/OneDrive/Documents/comp/Movies123forMe/lr_model.sav"
@@ -48,7 +35,7 @@ def predict(dataa):
     svm = joblib.load(svm_filename) # pylint: disable=C0103
     return logreg.predict(data), lr.predict(data), rf.predict(data), et.predict(data), dtc.predict(data), svm.predict(data) # pylint: disable=C0301
 
-def submit_form():
+def submit_form(): # pylint: disable=R0914, R0912, R0915
     """Function to create the movie prediction form."""
     # first section
     prod_budget = st.slider('What kind of production budget do you want for your movie?', min_value=0, max_value=525000000, value=1000000, step=100000) # pylint: disable=C0301
@@ -58,27 +45,27 @@ def submit_form():
 
     # second section
     genre_choices = {0: 'No', 1: 'Yes'}
-    genre_Action = st.selectbox("Do you want your movie to be an Action movie?", list(genre_choices.keys()), format_func=lambda x: genre_choices[x]) # pylint: disable=C0301
-    genre_Comedy = st.selectbox("Do you want your movie to be a Comedy movie?", list(genre_choices.keys()), format_func=lambda x: genre_choices[x]) # pylint: disable=C0301
-    genre_Drama = st.selectbox("Do you want your movie to be a Drama movie?", list(genre_choices.keys()), format_func=lambda x: genre_choices[x]) # pylint: disable=C0301
-    genre_Adventure = st.selectbox("Do you want your movie to be an Adventure movie?", list(genre_choices.keys()), format_func=lambda x: genre_choices[x]) # pylint: disable=C0301
-    genre_BlackComedy = st.selectbox("Do you want your movie to be a Black Comedy movie?", list(genre_choices.keys()), format_func=lambda x: genre_choices[x]) # pylint: disable=C0301
-    genre_Concert = st.selectbox("Do you want your movie to be a Concert/Performance movie?", list(genre_choices.keys()), format_func=lambda x: genre_choices[x]) # pylint: disable=C0301
-    genre_Documentary = st.selectbox("Do you want your movie to be a Documentary movie?", list(genre_choices.keys()), format_func=lambda x: genre_choices[x]) # pylint: disable=C0301
-    genre_Horror = st.selectbox("Do you want your movie to be a Horror movie?", list(genre_choices.keys()), format_func=lambda x: genre_choices[x]) # pylint: disable=C0301
-    genre_Musical = st.selectbox("Do you want your movie to be a Musical movie?", list(genre_choices.keys()), format_func=lambda x: genre_choices[x]) # pylint: disable=C0301
-    genre_RomanticComedy = st.selectbox("Do you want your movie to be a Romantic Comedy movie?", list(genre_choices.keys()), format_func=lambda x: genre_choices[x]) # pylint: disable=C0301
-    genre_Thriller = st.selectbox("Do you want your movie to be a Thriller/Suspense movie?", list(genre_choices.keys()), format_func=lambda x: genre_choices[x]) # pylint: disable=C0301
-    genre_Western = st.selectbox("Do you want your movie to be a Western movie?", list(genre_choices.keys()), format_func=lambda x: genre_choices[x]) # pylint: disable=C0301
+    genre_Action = st.selectbox("Do you want your movie to be an Action movie?", list(genre_choices.keys()), format_func=lambda x: genre_choices[x]) # pylint: disable=C0301, C0103
+    genre_Comedy = st.selectbox("Do you want your movie to be a Comedy movie?", list(genre_choices.keys()), format_func=lambda x: genre_choices[x]) # pylint: disable=C0301, C0103
+    genre_Drama = st.selectbox("Do you want your movie to be a Drama movie?", list(genre_choices.keys()), format_func=lambda x: genre_choices[x]) # pylint: disable=C0301, C0103
+    genre_Adventure = st.selectbox("Do you want your movie to be an Adventure movie?", list(genre_choices.keys()), format_func=lambda x: genre_choices[x]) # pylint: disable=C0301, C0103
+    genre_BlackComedy = st.selectbox("Do you want your movie to be a Black Comedy movie?", list(genre_choices.keys()), format_func=lambda x: genre_choices[x]) # pylint: disable=C0301, C0103
+    genre_Concert = st.selectbox("Do you want your movie to be a Concert/Performance movie?", list(genre_choices.keys()), format_func=lambda x: genre_choices[x]) # pylint: disable=C0301, C0103
+    genre_Documentary = st.selectbox("Do you want your movie to be a Documentary movie?", list(genre_choices.keys()), format_func=lambda x: genre_choices[x]) # pylint: disable=C0301, C0103
+    genre_Horror = st.selectbox("Do you want your movie to be a Horror movie?", list(genre_choices.keys()), format_func=lambda x: genre_choices[x]) # pylint: disable=C0301, C0103
+    genre_Musical = st.selectbox("Do you want your movie to be a Musical movie?", list(genre_choices.keys()), format_func=lambda x: genre_choices[x]) # pylint: disable=C0301, C0103
+    genre_RomanticComedy = st.selectbox("Do you want your movie to be a Romantic Comedy movie?", list(genre_choices.keys()), format_func=lambda x: genre_choices[x]) # pylint: disable=C0301, C0103
+    genre_Thriller = st.selectbox("Do you want your movie to be a Thriller/Suspense movie?", list(genre_choices.keys()), format_func=lambda x: genre_choices[x]) # pylint: disable=C0301, C0103
+    genre_Western = st.selectbox("Do you want your movie to be a Western movie?", list(genre_choices.keys()), format_func=lambda x: genre_choices[x]) # pylint: disable=C0301, C0103
 
 
     sequel_choices = {0: 'No', 1: 'Yes'}
-    sequel = st.selectbox("Do you want your movie to be a sequel?", list(sequel_choices.keys()), format_func=lambda x: sequel_choices[x]) # pylint: disable=C0301
+    sequel = st.selectbox("Do you want your movie to be a sequel?", list(sequel_choices.keys()), format_func=lambda x: sequel_choices[x]) # pylint: disable=C0301, C0103
     # submit button
-    clickSubmit = st.button('Predict success of your movie!')
+    clickSubmit = st.button('Predict success of your movie!') # pylint: disable=C0103
 
     if clickSubmit:
-        df = np.array([[prod_budget, rating, sequel, genre_Action,
+        df = np.array([[prod_budget, rating, sequel, genre_Action, # pylint: disable=C0103
         genre_Comedy, genre_Drama, genre_Adventure,
         genre_BlackComedy, genre_Concert, genre_Documentary,
         genre_Horror, genre_Musical, genre_RomanticComedy,
@@ -108,44 +95,44 @@ def submit_form():
         st.subheader("Movie Success Results:")
         for i in result_logreg:
             if i[0].round() == 0:
-                st.write(f"The Logistic Regression Machine Learning model predicted your movie would NOT BE A SUCCESS with a { (100 * score_logreg) }% confidence! :(") # pylint: disable=C0303
+                st.write(f"The Logistic Regression Machine Learning model predicted your movie would NOT BE A SUCCESS with a { (100 * score_logreg) }% confidence! :(") # pylint: disable=C0301
             elif i[0].round() == 1:
-                st.write(f"The Logistic Regression Machine Learning model predicted your movie would BE A SUCCESS with a { (100 * score_logreg) }% confidence! :)") # pylint: disable=C0303
+                st.write(f"The Logistic Regression Machine Learning model predicted your movie would BE A SUCCESS with a { (100 * score_logreg) }% confidence! :)") # pylint: disable=C0301
             else:
                 st.write("Please try again later.")
         for i in result_lr:
             if i[0].round() == 0:
-                st.write(f"The Linear Regression Machine Learning model predicted your movie would NOT BE A SUCCESS with a { (100 * score_lr) }% confidence! :(") # pylint: disable=C0303
+                st.write(f"The Linear Regression Machine Learning model predicted your movie would NOT BE A SUCCESS with a { (100 * score_lr) }% confidence! :(") # pylint: disable=C0301
             elif i[0].round() == 1:
-                st.write(f"The Linear Regression Machine Learning model predicted your movie would BE A SUCCESS with a { (100 * score_lr) }% confidence! :)") # pylint: disable=C0303
+                st.write(f"The Linear Regression Machine Learning model predicted your movie would BE A SUCCESS with a { (100 * score_lr) }% confidence! :)") # pylint: disable=C0301
             else:
                 st.write("Please try again later.")
         for i in result_rf:
             if i[0].round() == 0:
-                st.write(f"The Random Forest Regressor Machine Learning model predicted your movie would NOT BE A SUCCESS with a { (100 * score_rf) }% confidence! :(") # pylint: disable=C0303
+                st.write(f"The Random Forest Regressor Machine Learning model predicted your movie would NOT BE A SUCCESS with a { (100 * score_rf) }% confidence! :(") # pylint: disable=C0301
             elif i[0].round() == 1:
-                st.write(f"The Random Forest Regressor Machine Learning model predicted your movie would BE A SUCCESS with a { (100 * score_rf) }% confidence! :)") # pylint: disable=C0303
+                st.write(f"The Random Forest Regressor Machine Learning model predicted your movie would BE A SUCCESS with a { (100 * score_rf) }% confidence! :)") # pylint: disable=C0301
             else:
                 st.write("Please try again later.")
         for i in result_et:
             if i[0].round() == 0:
-                st.write(f"The Extra Tree Regressor Machine Learning model predicted your movie would NOT BE A SUCCESS with a { (100 * score_et) }% confidence! :(") # pylint: disable=C0303
+                st.write(f"The Extra Tree Regressor Machine Learning model predicted your movie would NOT BE A SUCCESS with a { (100 * score_et) }% confidence! :(") # pylint: disable=C0301
             elif i[0].round() == 1:
-                st.write(f"The Extra Tree Regressor Machine Learning model predicted your movie would BE A SUCCESS with a { (100 * score_et) }% confidence! :)") # pylint: disable=C0303
+                st.write(f"The Extra Tree Regressor Machine Learning model predicted your movie would BE A SUCCESS with a { (100 * score_et) }% confidence! :)") # pylint: disable=C0301
             else:
                 st.write("Please try again later.")
         for i in result_dtc:
             if i[0].round() == 0:
-                st.write(f"The Decision Tree Machine Learning model predicted your movie would NOT BE A SUCCESS with a { (100 * score_dtc) }% confidence! :(") # pylint: disable=C0303
+                st.write(f"The Decision Tree Machine Learning model predicted your movie would NOT BE A SUCCESS with a { (100 * score_dtc) }% confidence! :(") # pylint: disable=C0301
             elif i[0].round() == 1:
-                st.write(f"The Decision Tree Machine Learning model predicted your movie would BE A SUCCESS with a { (100 * score_dtc) }% confidence! :)") # pylint: disable=C0303
+                st.write(f"The Decision Tree Machine Learning model predicted your movie would BE A SUCCESS with a { (100 * score_dtc) }% confidence! :)") # pylint: disable=C0301
             else:
                 st.write("Please try again later.")
         for i in result_svm:
             if i[0].round() == 0:
-                st.write(f"The Support Vector Machine Learning model predicted your movie would NOT BE A SUCCESS with a { (100 * score_svm) }% confidence! :(") # pylint: disable=C0303
+                st.write(f"The Support Vector Machine Learning model predicted your movie would NOT BE A SUCCESS with a { (100 * score_svm) }% confidence! :(") # pylint: disable=C0301
             elif i[0].round() == 1:
-                st.write(f"The Support Vector Machine Learning model predicted your movie would BE A SUCCESS with a { (100 * score_svm) }% confidence! :)") # pylint: disable=C0303
+                st.write(f"The Support Vector Machine Learning model predicted your movie would BE A SUCCESS with a { (100 * score_svm) }% confidence! :)") # pylint: disable=C0301
             else:
                 st.write("Please try again later.")
 
@@ -178,43 +165,44 @@ def search_movies():
 
     if title:
         url = f'http://www.omdbapi.com/?t={title}&apikey=a98f1e4b'
-        re = requests.get(url)
-        re = re.json()
+        req = requests.get(url)
+        req = req.json()
         col1, col2 = st.columns([1, 2])
         with col1:
-            st.image(re['Poster'])
+            st.image(req['Poster'])
         with col2:
             st.subheader(re['Title'])
-            st.caption(f"Genre: {re['Genre']} | Year: {re['Year']} | Rated: {re['Rated']} | Released: {re['Released']}") # pylint: disable=C0303
-            st.write(re['Plot'])
-            st.text(f"Rating: {re['imdbRating']}")
-            st.progress(float(re['imdbRating']) / 10)
-        with open('response.json', 'w') as json_file:
-            json.dump(re, json_file)
-        with open('response.json', 'r') as file:
+            st.caption(f"Genre: {req['Genre']} | Year: {req['Year']} | Rated: {req['Rated']} | Released: {req['Released']}") # pylint: disable=C0301
+            st.write(req['Plot'])
+            st.text(f"Rating: {req['imdbRating']}")
+            st.progress(float(req['imdbRating']) / 10)
+        with open('response.json', 'w', encoding='utf-8') as json_file:
+            json.dump(req, json_file)
+        with open('response.json', 'r', encoding='utf-8') as file:
             json.loads(file.read())
         with open('response.json', encoding='utf-8') as inputfile:
             df = pd.read_json(inputfile)
-        open('movie_search.csv', 'a').write(df.to_csv(header = False, index=False))
+        open('movie_search.csv', 'a', encoding='utf-8').write(df.to_csv(header = False, index=False)) # pylint: disable=R1732
         duplicates = pd.read_csv('movie_search.csv', on_bad_lines='skip')
-        open('movie_clean.csv', 'a', encoding='utf-8').write(duplicates.to_csv(header = False, index=False)) # pylint: disable=C0303
+        open('movie_clean.csv', 'a', encoding='utf-8').write(duplicates.to_csv(header = False, index=False)) # pylint: disable=C0301, R7132
 
     if len(title) == 0:
-        url = f'http://www.omdbapi.com/?t=clueless&apikey=a98f1e4b'
-        re = requests.get(url)
-        re = re.json()
+        url = 'http://www.omdbapi.com/?t=clueless&apikey=a98f1e4b'
+        req = requests.get(url)
+        req = req.json()
         col1, col2 = st.columns([1, 2])
         with col1:
-            st.image(re['Poster'])
+            st.image(req['Poster'])
         with col2:
-            st.subheader(re['Title'])
-            st.caption(f"Genre: {re['Genre']} | Year: {re['Year']} | Rated: {re['Rated']} | Released: {re['Released']}") # pylint: disable=C0303
-            st.write(re['Plot'])
-            st.text(f"Rating: {re['imdbRating']}")
-            st.progress(float(re['imdbRating']) / 10)
+            st.subheader(req['Title'])
+            st.caption(f"Genre: {req['Genre']} | Year: {req['Year']} | Rated: {req['Rated']} | Released: {req['Released']}") # pylint: disable=C0301
+            st.write(req['Plot'])
+            st.text(f"Rating: {req['imdbRating']}")
+            st.progress(float(req['imdbRating']) / 10)
 
 
 def interface():
+    """Main interface for the app."""
     choices = ['Movie Search', 'Predict Success of a Movie!', 'What Movie Should You Watch?']
     success = st.sidebar.selectbox("Select a Movie Experience :)", choices)
     if success == 'Movie Search':
