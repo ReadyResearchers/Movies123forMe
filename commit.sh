102f377f9b2192a17cf0b6f6a912af31b6fc8dac
Adriana Solis <60237877+solisa986@users.noreply.github.com>
2023-03-24 18:31:50 -0400

    autoupdate 2023-03-24-18:31:50

--- commit.sh
+++ commit.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-git clone https://github.com/solisa986/Movies123forMe.git
-cd Movies123forMe
+git init
 git add .
 git commit -m "autoupdate `date +%F-%T`"
-git push origin main --force
+git push https://github.com/solisa986/Movies123forMe.git main --force
