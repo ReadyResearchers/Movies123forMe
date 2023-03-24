65f5777e61ff2996b272ffd927adf95bb21e80d7
Adriana Solis <60237877+solisa986@users.noreply.github.com>
2023-03-24 19:18:00 -0400

    updating

--- commit.sh
+++ commit.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-git add .
-git commit -m "autoupdate `date +%F-%T`"
+# git init
+# git add .
+git commit -a -m "autoupdate `date +%F-%T`"
 git push https://github.com/solisa986/Movies123forMe.git main --force
