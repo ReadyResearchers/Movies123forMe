462742045ef3b5c06c264caad66b35b957b7a394
Adriana Solis <60237877+solisa986@users.noreply.github.com>
2023-03-24 19:22:24 -0400

    seeing if this will work

--- commit.sh
+++ commit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-git clone https://github.com/solisa986/Movies123forMe.git
-cd Movies123forMe
+# git clone https://github.com/solisa986/Movies123forMe.git
+# cd Movies123forMe
 git init
 git add .
 git commit -m "autoupdate `date +%F-%T`"
