c2a1ca081ae5a621a7f1c43621696ee8719e946b
Adriana Solis <60237877+solisa986@users.noreply.github.com>
2023-03-23 23:09:02 -0400

    fixing trailing whitespaces in code

--- main.py
+++ main.py
@@ -202,7 +202,7 @@ def search_movies():
 
 def interface():
     """Main interface for the app."""
-    choices = ['Movie Search', 'Predict Success of a Movie!', 
+    choices = ['Movie Search', 'Predict Success of a Movie!',
     'What Movie Should You Watch?', 'Predict Movie Success with Text']
     success = st.sidebar.selectbox("Select a Movie Experience :)", choices)
     if success == 'Movie Search':
