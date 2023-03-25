15fa235dcb884a50ebe36219ac9124d3c18a07db
Adriana Solis <60237877+solisa986@users.noreply.github.com>
2023-03-25 19:48:14 -0400

    creating demo option

--- pages/regressions.py
+++ pages/regressions.py
@@ -32,51 +32,86 @@ def regression():
         #if i == 1:
             #print(opus['production_budget'].mean())
     # col = opus.columns
-    columns1= {1: opus.columns[0], 2: opus.columns[1], 3: opus.columns[2],
-        4: opus.columns[3], 5: opus.columns[4], 6: opus.columns[5],
-        7: opus.columns[6], 8: opus.columns[7], 9: opus.columns[8],
-        10: opus.columns[9], 11: opus.columns[10], 12: opus.columns[11],
-        13: opus.columns[12], 14: opus.columns[13], 15: opus.columns[14],
-        16: opus.columns[15], 17: opus.columns[16], 18: opus.columns[17],
-        19: opus.columns[18]}
-    columns_num = {1: opus.columns[1], 2: opus.columns[2], 3: opus.columns[3], 
-        4: opus.columns[4], 5: opus.columns[5], 6: opus.columns[6], 7: opus.columns[10],
-        8: opus.columns[11],
-        9: opus.columns[12], 10: opus.columns[13], 11: opus.columns[14],
-        12: opus.columns[15], 13: opus.columns[16], 14: opus.columns[17],
-        15: opus.columns[18]}
-    category = st.sidebar.selectbox("Select a category to analyze", list(columns1.keys()), format_func=lambda x: columns1[x])
-    x_val = st.sidebar.selectbox("X-value: ", list(columns_num.keys()), format_func=lambda x: columns_num[x])
-    y_val = st.sidebar.selectbox("Y-val: ", list(columns_num.keys()), format_func=lambda x: columns_num[x])
-    if x_val == y_val:
-        x_val = opus.columns[1]
-        y_val = opus.columns[4]
-        # category = opus.columns[6]
+    use_demo = st.sidebar.checkbox('Use Demo of Regression:')
+    if use_demo:
+        # scatterplot chart
+        fig = px.scatter(opus, x = opus['genre_BlackComedy'], y = opus['movie_success'], color = opus['rating'], 
+                        trendline = 'ols', title = f"Scatterplot of {y_val} on {x_val}")
+        fig.data[1].line.color = 'red'
+        fig.update_xaxes(rangeslider_visible = True)
+        st.plotly_chart(fig)
+        res = px.get_trendline_results(fig)
+        trendline = res['px_fit_results'].iloc[0]
+        st.write(trendline.summary())
+        # pie chart
+        fig1 = px.pie(opus, values = opus['genre_BlackComedy'], names = opus['movie_success'], 
+                    title = f'Percentage of movie "{y_val}" in "{x_val}"')
+        fig1.update_traces(textinfo="percent+value")
+        st.plotly_chart(fig1)
 
-    # scatterplot chart
-    fig = px.scatter(opus, x = x_val, y = y_val, color = columns1[category], 
-                    trendline = 'ols', title = f"Scatterplot of {y_val} on {x_val}")
-    fig.data[1].line.color = 'red'
-    fig.update_xaxes(rangeslider_visible = True)
-    st.plotly_chart(fig)
-    res = px.get_trendline_results(fig)
-    trendline = res['px_fit_results'].iloc[0]
-    st.write(trendline.summary())
-    # pie chart
-    fig1 = px.pie(opus, values = x_val, names = y_val, 
-                title = f'Percentage of movie "{y_val}" in "{x_val}"')
-    fig1.update_traces(textinfo="percent+value")
-    st.plotly_chart(fig1)
+        # bar chart
+        fig2 = px.bar(opus, x = opus['genre_BlackComedy'], y = opus['movie_success'], color = opus['rating'],
+        title = f'Bar chart of {x_val} on {y_val}')
+        st.plotly_chart(fig2)
 
-    # bar chart
-    fig2 = px.bar(opus, x = x_val, y = y_val, color = columns1[category],
-    title = f'Bar chart of {x_val} on {y_val}')
-    st.plotly_chart(fig2)
+        #might need to work on this to get the func to work
+        fig3 = px.bar(opus, opus['genre_BlackComedy'], color = opus['rating'],
+        title = f'Bar chart of the count of {x_val}')
+        st.plotly_chart(fig3)
+        show_stat = st.sidebar.checkbox('Show Summary statistics:')
+        if show_stat:
+            res = px.get_trendline_results(fig)
+            trendline = res['px_fit_results'].iloc[0]
+            st.write(trendline.summary())
+    else:
+        columns1= {1: opus.columns[0], 2: opus.columns[1], 3: opus.columns[2],
+            4: opus.columns[3], 5: opus.columns[4], 6: opus.columns[5],
+            7: opus.columns[6], 8: opus.columns[7], 9: opus.columns[8],
+            10: opus.columns[9], 11: opus.columns[10], 12: opus.columns[11],
+            13: opus.columns[12], 14: opus.columns[13], 15: opus.columns[14],
+            16: opus.columns[15], 17: opus.columns[16], 18: opus.columns[17],
+            19: opus.columns[18]}
+        columns_num = {1: opus.columns[1], 2: opus.columns[2], 3: opus.columns[3], 
+            4: opus.columns[4], 5: opus.columns[5], 6: opus.columns[6], 7: opus.columns[10],
+            8: opus.columns[11],
+            9: opus.columns[12], 10: opus.columns[13], 11: opus.columns[14],
+            12: opus.columns[15], 13: opus.columns[16], 14: opus.columns[17],
+            15: opus.columns[18]}
+        category = st.sidebar.selectbox("Select a category to analyze", list(columns1.keys()), format_func=lambda x: columns1[x])
+        x_val = st.sidebar.selectbox("X-value: ", list(columns_num.keys()), format_func=lambda x: columns_num[x])
+        y_val = st.sidebar.selectbox("Y-val: ", list(columns_num.keys()), format_func=lambda x: columns_num[x])
+        if x_val == y_val:
+            x_val = opus.columns[1]
+            y_val = opus.columns[4]
+            category = opus.columns[6]
 
-    #might need to work on this to get the func to work
-    fig3 = px.bar(opus, x = x_val, color = columns1[category],
-    title = f'Bar chart of the count of {x_val}')
-    st.plotly_chart(fig3)
+        # scatterplot chart
+        fig = px.scatter(opus, x = x_val, y = y_val, color = columns1[category], 
+                        trendline = 'ols', title = f"Scatterplot of {y_val} on {x_val}")
+        fig.data[1].line.color = 'red'
+        fig.update_xaxes(rangeslider_visible = True)
+        st.plotly_chart(fig)
+        # pie chart
+        fig1 = px.pie(opus, values = x_val, names = y_val, 
+                    title = f'Percentage of movie "{y_val}" in "{x_val}"')
+        fig1.update_traces(textinfo="percent+value")
+        st.plotly_chart(fig1)
+
+        # bar chart
+        fig2 = px.bar(opus, x = x_val, y = y_val, color = columns1[category],
+        title = f'Bar chart of {x_val} on {y_val}')
+        st.plotly_chart(fig2)
+
+        #might need to work on this to get the func to work
+        fig3 = px.bar(opus, x = x_val, color = columns1[category],
+        title = f'Bar chart of the count of {x_val}')
+        st.plotly_chart(fig3)
+
+        show_stat = st.sidebar.checkbox('Show Summary statistics:')
+        if show_stat:
+            res = px.get_trendline_results(fig)
+            trendline = res['px_fit_results'].iloc[0]
+            st.write(trendline.summary())
 
 regression()
 #os.remove(html_fname)
