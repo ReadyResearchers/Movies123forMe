67b713088ee6032da227317f6c4a016d8121f6fc
Adriana Solis <60237877+solisa986@users.noreply.github.com>
2023-03-25 18:10:08 -0400

    trying to render pdf file

--- main_dashboard.py
+++ main_dashboard.py
@@ -36,8 +36,9 @@ def main():
     pdf_file = "pages/SeniorThesis.pdf"
     with open(pdf_file,"rb") as f:
       base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-    pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
+    pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
     st.markdown(pdf_display, unsafe_allow_html=True)
+    
     # displaying the descriptions of every page
     intro_markdown = read_markdown_file()
     st.markdown(intro_markdown, unsafe_allow_html=True)
