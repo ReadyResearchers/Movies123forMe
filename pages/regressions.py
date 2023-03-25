b046d64efafe0b93b14ea2f2d6238d0fe1c5847b
Adriana Solis <60237877+solisa986@users.noreply.github.com>
2023-03-25 19:39:12 -0400

    changing key for regressions

--- pages/regressions.py
+++ pages/regressions.py
@@ -31,8 +31,8 @@ def regression():
     #for i in opus['movie_success']:
         #if i == 1:
             #print(opus['production_budget'].mean())
-    col = opus.columns
-    columns= {1: opus.columns[0], 2: opus.columns[1], 3: opus.columns[2],
+    # col = opus.columns
+    columns1= {1: opus.columns[0], 2: opus.columns[1], 3: opus.columns[2],
         4: opus.columns[3], 5: opus.columns[4], 6: opus.columns[5],
         7: opus.columns[6], 8: opus.columns[7], 9: opus.columns[8],
         10: opus.columns[9], 11: opus.columns[10], 12: opus.columns[11],
@@ -45,16 +45,16 @@ def regression():
         9: opus.columns[12], 10: opus.columns[13], 11: opus.columns[14],
         12: opus.columns[15], 13: opus.columns[16], 14: opus.columns[17],
         15: opus.columns[18]}
-    category = st.sidebar.selectbox("Select a category to analyze", list(columns.keys()), format_func=lambda x: columns[x])
+    category = st.sidebar.selectbox("Select a category to analyze", list(columns1.keys()), format_func=lambda x: columns1[x])
     x_val = st.sidebar.selectbox("X-value: ", list(columns_num.keys()), format_func=lambda x: columns_num[x])
     y_val = st.sidebar.selectbox("Y-val: ", list(columns_num.keys()), format_func=lambda x: columns_num[x])
     if x_val == y_val:
         x_val = opus.columns[1]
         y_val = opus.columns[4]
-        category = opus.columns[6]
+        # category = opus.columns[6]
 
     # scatterplot chart
-    fig = px.scatter(opus, x = x_val, y = y_val, color = columns[category], 
+    fig = px.scatter(opus, x = x_val, y = y_val, color = columns1[category], 
                     trendline = 'ols', title = f"Scatterplot of {y_val} on {x_val}")
     fig.data[1].line.color = 'red'
     fig.update_xaxes(rangeslider_visible = True)
@@ -69,12 +69,12 @@ def regression():
     st.plotly_chart(fig1)
 
     # bar chart
-    fig2 = px.bar(opus, x = x_val, y = y_val, color = columns[category],
+    fig2 = px.bar(opus, x = x_val, y = y_val, color = columns1[category],
     title = f'Bar chart of {x_val} on {y_val}')
     st.plotly_chart(fig2)
 
     #might need to work on this to get the func to work
-    fig3 = px.bar(opus, x = x_val, color = columns[category],
+    fig3 = px.bar(opus, x = x_val, color = columns1[category],
     title = f'Bar chart of the count of {x_val}')
     st.plotly_chart(fig3)
 
